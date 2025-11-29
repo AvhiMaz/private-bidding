3f5a23a118a8c551218e49303c9511890c8f2b15
AvhiMaz <avhimazumder5@outlook.com>
2025-11-29 15:21:26 +0530

    feat(error): add more error code
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- programs/private-bidding/src/error.rs
+++ programs/private-bidding/src/error.rs
@@ -10,4 +10,40 @@ pub enum ErrorCode {
     BidTooLow,
     #[msg("Seller cannot bid on own auction")]
     SellerCannotBid,
+    #[msg("No bids placed on auction")]
+    NoBids,
+    #[msg("Auction already settled")]
+    AuctionAlreadySettled,
+    #[msg("Invalid seller account")]
+    InvalidSeller,
+    #[msg("Invalid mint for escrow account")]
+    InvalidMint,
+    #[msg("Insufficient funds in escrow account")]
+    InsufficientEscrow,
+    #[msg("Invalid winner token account")]
+    InvalidWinnerTokenAccount,
+    #[msg("Invalid auction state")]
+    InvalidAuctionState,
+    #[msg("Unauthorized action")]
+    Unauthorized,
+    #[msg("Overflow occurred")]
+    Overflow,
+    #[msg("Underflow occurred")]
+    Underflow,
+    #[msg("Invalid bid account")]
+    InvalidBidAccount,
+     #[msg("Auction has not ended yet")]
+    AuctionNotEnded,
+    #[msg("No winner found for this auction")]
+    NoWinner,
+    #[msg("Invalid bid amount")]
+    InvalidBid,
+    #[msg("Winner account mismatch")]
+    WinnerMismatch,
+    #[msg("Math overflow occurred")]
+    MathOverflow,
+    #[msg("Auction has already been settled")]
+    AlreadySettled,
+    #[msg("Auction is still active")]
+    AuctionStillActive,
 }
