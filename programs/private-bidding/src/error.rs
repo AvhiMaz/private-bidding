c4f5f0e4db571c7b5dfd794225b131cb09346f47
AvhiMaz <avhimazumder5@outlook.com>
2025-11-29 13:25:24 +0530

    feat(error): add error code
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- programs/private-bidding/src/error.rs
+++ programs/private-bidding/src/error.rs
@@ -2,6 +2,12 @@ use anchor_lang::prelude::*;
 
 #[error_code]
 pub enum ErrorCode {
-    #[msg("Custom error message")]
-    CustomError,
+    #[msg("Auction has ended")]
+    AuctionEnded,
+    #[msg("Auction already settled")]
+    AuctionSettled,
+    #[msg("Bid amount too low")]
+    BidTooLow,
+    #[msg("Seller cannot bid on own auction")]
+    SellerCannotBid,
 }
