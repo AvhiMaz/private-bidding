f0808411d075dafcf587871fcf61a668f24bb8d9
AvhiMaz <avhimazumder5@outlook.com>
2025-11-29 13:27:10 +0530

    chore(pda): update bid escrow pds
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- programs/private-bidding/src/constants.rs
+++ programs/private-bidding/src/constants.rs
@@ -6,4 +6,4 @@ pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
 pub const AUCTION_SEED: &str = "auction";
 pub const ESCROW_SEED: &str = "escrow";
 pub const BID_SEED: &str = "bid";
-pub const BID_ESCROW_SEED: &str = "bid-escrow";
+pub const BID_ESCROW_SEED: &str = "bid_escrow";
